269f06887fdb825c4dc2d1b966ee530cbe5c0951
suryamaulana98 <suryamaulana757@gmail.com>
2026-06-10 21:40:30 +0700

    konfirugarasi production cloud database

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'hotel_reservation_db',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 4000,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
